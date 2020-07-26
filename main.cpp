bb29a5aa93250887f87bbce57240e4ae04997824
Beef-Charisma <luckyguy7523@gmail.com>
2020-07-26 06:24:27 -0700

    Add main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,33 @@ void usage() {
   printf("sample: pcap_test wlan0\n");
 }
 
+void printMAC(const u_char* data){
+  int i;
+  for(i=0; i<6; i++){
+    if(*(data+i)<16)
+      printf("0");
+    printf("%X", *(data+i));
+    if(i!=5)
+      printf(":");
+    else
+      printf("\n");
+  }
+}
+
+void printIP(const u_char* data){
+  for(int i=0;i<4;i++){
+    printf("%d",*(data+i));
+    if(i!=3) 
+      printf(".");
+    else
+      printf("\n");
+  }
+}
+
+void printPORT(const u_char* data){
+  printf("%u\n", (unsigned int)ntohs(*(uint16_t *)(data)));
+}
+
 int main(int argc, char* argv[]) {
   if (argc != 2) {
     usage();
@@ -26,15 +53,20 @@ int main(int argc, char* argv[]) {
     int res = pcap_next_ex(handle, &header, &packet);
     if (res == 0) continue;
     if (res == -1 || res == -2) break;
-    printf("Source MAC : %X%X%X%X%X%X\n", packet[6], packet[7], packet[8], packet[9], packet[10], packet[11]); 
-    printf("Destination MAC : %X%X%X%X%X%X\n", packet[0], packet[1], packet[2], packet[3], packet[4], packet[5]);
+    printf("Source MAC : ");
+    printMAC(packet+6);
+    printf("Destination MAC : ");
+    printMAC(packet);
     if(packet[12]==8 && packet[13]==0){
-    printf("Source IP : %X%X%X%X\n", packet[26], packet[27], packet[28], packet[29]);
-    printf("Destination IP : %X%X%X%X\n", packet[30], packet[31], packet[32], packet[33]);
-    printf("Source Port : %X%X\n", packet[34], packet[35]);
-    printf("Destination Port : %X%X\n", packet[36], packet[37]);
-  }
-
+      printf("Source IP : ");
+      printIP(packet+26);
+      printf("Destination IP : ");
+      printIP(packet+30);
+      printf("Source Port : ");
+      printPORT(packet+20);
+      printf("Destination Port : ");
+      printPORT(packet+22);
+    }
   }
 
   pcap_close(handle);
